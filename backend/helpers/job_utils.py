d09ffab0cfe58dcba624739f9762f927714adddd
PabloG55 <pasebarona@gmail.com>
2025-06-06 15:18:40 -0500

    Fixed telegram timezone

--- backend/helpers/job_utils.py
+++ backend/helpers/job_utils.py
@@ -12,8 +12,9 @@ logger = logging.getLogger(__name__)
 
 def schedule_jobs_for_task(task):
     """Schedules reminder and follow-up jobs for a given task."""
-    utc_reminder_time = task.scheduled_time.astimezone(pytz.utc)
+    utc_reminder_time = task.scheduled_time.astimezone(ECUADOR_TZ)
     followup_time = utc_reminder_time + timedelta(hours=1)
+    logger.info(f"utc_reminder_time: {utc_reminder_time}, followup_time: {followup_time}")
 
     reminder_id = f"reminder_{task.id}_{int(utc_reminder_time.timestamp())}"
     followup_id = f"followup_{task.id}_{int(followup_time.timestamp())}"
@@ -50,7 +51,7 @@ def remove_jobs_for_task(task_id):
             logger.info(f"Removed job: {job.id}")
 
 def schedule_still_working_tasks(task):
-    next_reminder_time = datetime.now(pytz.utc) + timedelta(hours=1)
+    next_reminder_time = datetime.now(ECUADOR_TZ) + timedelta(hours=1)
     reminder_id = f"followup_{task.id}_{int(next_reminder_time.timestamp())}"
 
     scheduler.add_job(
